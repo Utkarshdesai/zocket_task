1d53240870e3160dd9af85419e94f34534ec0cd4
Utkarsh <desaiutkarsh542@gmail.com>
2024-05-18 16:10:48 +0530

    setup vite-react project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p className='text-md bg-red-600 text-center '> hello </p>
+      <div className='w-[200px] h-[200px] bg-slate-300'>
+
+        <h2> park </h2>
+
+
+      </div>
+       
+    </>
+  )
+}
+
+export default App
